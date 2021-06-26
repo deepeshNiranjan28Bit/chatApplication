49efd489bace2331fd19ddb4ce83710f0ec95516
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-06-26 16:34:50 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import { Switch } from 'react-router';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ src/components/PublicRoute.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+
+function PublicRoute({ children, ...routeProps }) {
+  const profile = false;
+
+  if (profile) {
+    return <Redirect to="/" />;
+  }
+  return <Route {...routeProps}>{children}</Route>;
+}
+
+export default PublicRoute;
