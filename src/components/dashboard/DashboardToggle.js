00bf57482ca2e974b5ca312f83b367c2e046de8c
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-06-28 22:52:20 +0530

    responsed dashboard

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Button, Icon, Drawer } from 'rsuite';
 import Dashboard from '.';
-import { useModalState } from '../../misc/custom-hooks';
+import { useModalState, useMediaQuery } from '../../misc/custom-hooks';
 
 function DashboardToggle() {
   const { isOpen, close, open } = useModalState();
+  const is992px = useMediaQuery('(max-width: 992px)');
 
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard"> &ensp; Dashboard</Icon>
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={is992px} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
