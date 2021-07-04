c4e0312aff2c5eaecb0f0abbc741960410493f67
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-07-04 23:30:04 +0530

    styled-h-100

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Divider } from 'rsuite';
 import DashboardToggle from './dashboard/DashboardToggle';
 import CreateRoomBtnModal from './CreateRoomBtnModal';
+import ChatRoomList from './rooms/ChatRoomList';
 
 function Sidebar() {
+  const topSideBarRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (topSideBarRef.current) {
+      setHeight(topSideBarRef.current.scrollHeight);
+    }
+  }, [topSideBarRef]);
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSideBarRef}>
         <DashboardToggle />
         <CreateRoomBtnModal />
+        <Divider>Join the conversation</Divider>
       </div>
-      <div></div>
+      <div>
+        <ChatRoomList aboveElHeight={height} />
+      </div>
     </div>
   );
 }
