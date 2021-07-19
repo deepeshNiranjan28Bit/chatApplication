2eb20214eae87a8ed356e2202316cb1ca6120f04
webdesigner-28bit <deepeshniranjan@gmail.com>
2021-07-19 07:47:02 +0530

    added config

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
+//firebase config
 
 const config = {
   apiKey: 'AIzaSyBlP3JCACrgD_sklng-VkibDSo6GyHxnH0',
